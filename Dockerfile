2d3f38466d6cdd10ac68271818fa1cd88dbc17ed
Aditya <sanjeev0575@gmail.com>
2025-07-29 04:24:15 +0530

    adding the ecs to deploy

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 EXPOSE 5000
-CMD ["python", "app.py"]
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return 'Hello, Flask!'
 
 if __name__ == '__main__':
     app.run(debug=True)
