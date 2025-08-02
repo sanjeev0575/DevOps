6a18c343d15c1ea32907030bf42e70123d949955
Aditya <sanjeev0575@gmail.com>
2025-08-02 16:26:35 +0530

    flaskfile udpated

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def home():
     return 'Hello, Flask!'
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
