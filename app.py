4af5e3fefadaac66afb0cc6ce1d7548995c25621
Aditya <sanjeev0575@gmail.com>
2025-08-03 17:24:30 +0530

    dockerfile udpated for testing

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello, Flask!'
+
+# @app.route('/health')
+# def health():
+#     return 'OK', 200
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello from ECS!'
+
+# @app.route('/health')
+# def health():
+#     return 'OK', 200
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Flask!'
+    return 'Hello from ECS!'
 
 @app.route('/health')
 def health():
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
